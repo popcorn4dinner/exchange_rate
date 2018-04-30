e6fbc238af178732c51383090e3fab36229152ad
Bastian Schnieders <bastian.schnieders@stepstone.be>
2018-04-30 10:06:10 +0200

    squash! work in progress - tests

--- spec/exchange_rate_spec.rb
+++ spec/exchange_rate_spec.rb
@@ -3,6 +3,20 @@ require 'date'
 
 describe ExchangeRate do
   it "should return the changerate for known currencies" do
-    expect(ExchangeRate.at(Date.today, 'EUR', 'SEK')).to be_a(String)
+    expect(ExchangeRate.at(Date.today, 'EUR', 'SEK')).to be_a(BigDecimal)
   end
+
+  it "should throw an exception for unknown currencies" do
+    expect(ExchangeRate.at(Date.tomorrow, 'EUR', 'SEK')).to be_a(BigDecimal)
+  end
+
+  it "should throw an exception if the exchangedAt date is not within the allowed timeframe" do
+    expect(ExchangeRate.at(Date.today, 'EUR', 'SEK')).to be_a(BigDecimal)
+  end
+
+  it "should throw and exception if malconfigured" do
+    expect(ExchangeRate.at(Date.today, 'EUR', 'SEK')).to be_a(BigDecimal)
+  end
+
+
 end
